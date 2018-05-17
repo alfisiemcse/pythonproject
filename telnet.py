cd0d5064b9d51f9d4ac51ac75207b8947db7e8f8
alfis <alfis.iemcse@gmail.com>
2018-05-17 13:27:18 +0000

    intro

--- telnet.py
+++ telnet.py
@@ -12,8 +12,14 @@ tn.write(user.encode('ascii') + b"\n")
 if password:
     tn.read_until(b"Password: ")
     tn.write(password.encode('ascii') + b"\n")
+tn.write(b"enable"+ b"\n")
+tn.write(b"cisco\n"+b"\n")
+tn.write(b"conf t\n"+b"\n")
+tn.write(b"int loopback 0\n"+b"\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n"+b"\n")
+tn.write(b"no shut"+b"\n")
+tn.write(b"end"+b"\n")
+tn.write(b"exit"+b"\n")
 
-tn.write(b"ls\n")
-tn.write(b"exit\n")
 
 print(tn.read_all().decode('ascii'))
